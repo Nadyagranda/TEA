2fa97e0a0e414586fc68aaca3cbce284d5045e29
Nadya Paula Granda Flores (24085) <nadya.granda@est.zamorano.edu>
2022-09-24 19:21:44 -0600

    Primera versión de Hola Mundo

---
+++ labs/lab01/lab01.py
@@ -0,0 +1,6 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+# Autor: Nadya Granda
+# Fecha: 2022.09.02
+# Editado: 2022.09.02
+
+print("Hola, Nadya!")
---
+++ labs/lab04/temporal07.py
@@ -0,0 +1,13 @@
+while True: 
+numero = int(input("Ingrese un numero: "))
+if (numero == 1):
+    print("Uno")
+elif (numero == 2):
+    print("Dos")
+elif (numero == 3):
+    print("Tres")
+elif (numero == 4):
+    print("Cuatro")
+elif (numero == 5):
+    print(" Cinco")
+else:
